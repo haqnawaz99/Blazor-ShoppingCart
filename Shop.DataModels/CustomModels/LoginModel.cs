7e7532c37094ee69c7295206d93e5c308e32677a
Haq Nawaz <haqnawaz99@yahoo.com>
2021-08-21 23:16:46 +0500

    login design basic work done

---
+++ Shop.DataModels/CustomModels/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shop.DataModels.CustomModels
+{
+    public class LoginModel
+    {
+        public string UserKey { get; set; }
+
+        public string Name { get; set; }
+
+        [Required (ErrorMessage ="Email is required")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
